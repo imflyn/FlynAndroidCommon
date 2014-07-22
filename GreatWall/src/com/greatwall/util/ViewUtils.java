6a486277b2d1ecb13a5561656cf6ee2e31a327f7
imflyn <imflyn@163.com>
2014-07-22 10:47:15 +0800

    1

--- GreatWall/src/com/greatwall/util/ViewUtils.java
+++ GreatWall/src/com/greatwall/util/ViewUtils.java
@@ -69,6 +69,10 @@ public class ViewUtils
         viewGroup.setBackgroundDrawable(null);
     }
 
+    /**
+     * 修复键盘内存泄露
+     * @param destContext
+     */
     public static void fixInputMethodManagerLeak(Context destContext)
     {
         if (destContext == null)
