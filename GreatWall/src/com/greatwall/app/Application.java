95dcbfab17a02776a70c69b89cdd0c9820f2cf6d
imflyn <imflyn@163.com>
2014-07-28 21:04:47 +0800

    1

--- GreatWall/src/com/greatwall/app/Application.java
+++ GreatWall/src/com/greatwall/app/Application.java
@@ -22,7 +22,7 @@ public class Application extends android.app.Application
 
     @Override
     public void onCreate()
-    {
+    { 
         super.onCreate();
         mContext = this;
         init();
