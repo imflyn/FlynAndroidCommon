430c306a4f5a23ded33a679ed50679dd9718cd8b
imflyn <imflyn@163.com>
2014-08-02 21:07:10 +0800

    1

--- GreatWall/src/com/greatwall/ui/BaseActionBarActivity.java
+++ GreatWall/src/com/greatwall/ui/BaseActionBarActivity.java
@@ -63,17 +63,6 @@ public abstract class BaseActionBarActivity extends ActionBarActivity implements
         }
     }
 
-    private void reload()
-    {
-        Intent intent = getIntent();
-        overridePendingTransition(0, 0);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        finish();
-
-        overridePendingTransition(0, 0);
-        startActivity(intent);
-    }
-
     @Override
     protected void onPause()
     {
