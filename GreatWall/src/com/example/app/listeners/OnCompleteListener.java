951b02b6887304f2d0c75926aeb54f191b2d78a3
imflyn <imflyn@163.com>
2014-08-09 14:17:20 +0800

    1

---
+++ GreatWall/src/com/example/app/listeners/OnCompleteListener.java
@@ -0,0 +1,6 @@
+package com.example.app.listeners;
+
+public interface OnCompleteListener<T>
+{
+    public void onComplete(T data);
+}
---
+++ GreatWall/src/com/example/app/listeners/OnUpdateListener.java
@@ -0,0 +1,6 @@
+package com.example.app.listeners;
+
+public interface OnUpdateListener<T>
+{
+    public void onUpdate(Object... data);
+}
