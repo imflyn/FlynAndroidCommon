f4aced0c1d15835664b962e6ee4f33676ab04f72
imflyn <imflyn@users.noreply.github.com>
2014-09-30 09:54:08 +0800

    AsyncTask

---
+++ GreatWall/src/com/greatwall/ui/utils/SafeAsyncTask.java
@@ -0,0 +1,53 @@
+package com.greatwall.ui.utils;
+
+public abstract class SafeAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+
+	private Exception mError;
+
+	@Override
+	public Result doInBackground(Params... params) {
+		try {
+			return run(params);
+		} catch (Exception e) {
+			mError = e;
+			return null;
+		}
+	}
+
+	@Override
+	public void onPostExecute(Result result) {
+		if (isCancelled())
+			return;
+
+		try {
+			if (mError != null) {
+				onSuccess(result);
+			} else {
+				onFailure(mError);
+			}
+		} finally {
+
+			if (isCancelled())
+				return;
+			onFinish();
+		}
+	}
+
+	protected abstract Result run(Params... params);
+
+	public abstract void onSuccess(Result result);
+
+	public abstract void onFailure(Exception e);
+
+	public void onFinish() {
+
+	}
+
+	public static boolean isRunning(SafeAsyncTask<?, ?, ?> asyncTask) {
+		return asyncTask != null && Status.RUNNING == asyncTask.getStatus();
+	}
+
+	public static boolean isFinished(SafeAsyncTask<?, ?, ?> asyncTask) {
+		return asyncTask == null || Status.FINISHED == asyncTask.getStatus();
+	}
+}
